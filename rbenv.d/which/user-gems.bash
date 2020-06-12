533db51814dafd8f45a51bb4427d1d088f2bf593
Sernin van de Krol <serninpc@paneidos.net>
2020-06-12 21:33:23 +0200

    mprove implementation of which user-gems plugin

--- rbenv.d/which/user-gems.bash
+++ rbenv.d/which/user-gems.bash
@@ -1,41 +1,40 @@
+# List all gem bindirs for specified ruby executable
+list_gem_bindirs() {
+  local ruby="$1"
+  "$ruby" -e \
+          'puts Gem.path.map(&Gem.method(:bindir)).join("\n")' 2>/dev/null || true
+}
+
 if [ "$RBENV_VERSION" == "system" ]; then
   PATH="$(remove_from_path "${RBENV_ROOT}/shims")" \
-  gem=("$(command -v "gem" || true)")
+  gem="$(command -v "gem" || true)" \
+  ruby="$(command -v "ruby" || true)"
   if [ -x "$gem" ]; then
     OLDIFS="$IFS"
-    IFS=':' gempaths=(`$gem environment gempath`)
+    IFS=$'\n' bindirs=(`list_gem_bindirs "$ruby"`)
     IFS="$OLDIFS"
-    for gempath in "${gempaths[@]}"; do
-      path="${gempath}/bin/${RBENV_COMMAND}"
-      if [ -x "$path" ]; then
-        RBENV_COMMAND_PATHS+=("$path")
-      fi
+    for bindir in "${bindirs[@]}"; do
+      RBENV_COMMAND_PATHS+=("${bindir}/${RBENV_COMMAND}")
     done
   fi
 else
   gem="${RBENV_ROOT}/versions/${RBENV_VERSION}/bin/gem"
   if [ -x "$gem" ]; then
     OLDIFS="$IFS"
-    IFS=':' gempaths=(`$gem environment gempath`)
+    IFS=$'\n' bindirs=(`list_gem_bindirs "${RBENV_ROOT}/versions/${RBENV_VERSION}/bin/ruby"`)
     IFS="$OLDIFS"
-    for gempath in "${gempaths[@]}"; do
-      path="${gempath}/bin/${RBENV_COMMAND}"
-      if [ -x "$path" ]; then
-        RBENV_COMMAND_PATHS+=("$path")
-      fi
+    for bindir in "${bindirs[@]}"; do
+      RBENV_COMMAND_PATHS+=("${bindir}/${RBENV_COMMAND}")
     done
   fi
   if [ -n "$RBENV_SYSTEM_VERSIONS_DIR" ]; then
     gem="${RBENV_SYSTEM_VERSIONS_DIR}/${RBENV_VERSION}/bin/gem"
     if [ -x "$gem" ]; then
       OLDIFS="$IFS"
-      IFS=':' gempaths=(`$gem environment gempath`)
+      IFS=$'\n' bindirs=(`list_gem_bindirs "${RBENV_SYSTEM_VERSIONS_DIR}/${RBENV_VERSION}/bin/ruby"`)
       IFS="$OLDIFS"
-      for gempath in "${gempaths[@]}"; do
-        path="${gempath}/bin/${RBENV_COMMAND}"
-        if [ -x "$path" ]; then
-          RBENV_COMMAND_PATHS+=("$path")
-        fi
+      for bindir in "${bindirs[@]}"; do
+        RBENV_COMMAND_PATHS+=("${bindir}/${RBENV_COMMAND}")
       done
     fi
   fi

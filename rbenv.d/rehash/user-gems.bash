5ed2027da3d9b7fb01598bea3a7759852379fa0b
Sernin van de Krol <serninpc@paneidos.net>
2020-06-19 14:05:26 +0200

    Add suppport for gem commands in home (instead of only in the Ruby installation's bin dir) (#1)
    Add support for gems installed in users' home directories
    
    `rbenv exec`, `rbenv rehash` etc. only looked for gem commands in the Ruby installation's 'bin' directory. In other words, /usr/lib/rbenv/versions/xxx/bin/foo. With this change, rbenv looks for gem commands in the user's home directory too (~/.gem/ruby/xxx/bin).
    
    Merges fullstaq/labs/fulstaq-rbenv pull request #1.

---
+++ rbenv.d/rehash/user-gems.bash
@@ -0,0 +1,32 @@
+# This plugin lists all available gem commands for the current user
+# and creates shims for these commands.
+# It works by iterating over all installed version and gathering the list
+# of executables. For each gem path of the installed Ruby version,
+# we ask Rubygems for the corresponding bin path. For each bin path
+# we list the executable files.
+
+list_executable_names() {
+  local version file
+  local -a version_dirs
+  rbenv-versions --bare --skip-aliases | \
+  while read -r version; do
+    version_dirs=("${RBENV_ROOT}/versions/${version}")
+    if [ -n "$RBENV_SYSTEM_VERSIONS_DIR" ]; then
+      version_dirs+=("$RBENV_SYSTEM_VERSIONS_DIR/${version}")
+    fi
+    for version_dir in "${version_dirs[@]}"; do
+      gem="${version_dir}/bin/gem"
+      if [ -x "$gem" ]; then
+        "${version_dir}/bin/ruby" -e \
+          'puts Gem.path.map(&Gem.method(:bindir)).join("\n")' 2>/dev/null | \
+        while read -r bindir; do
+          for file in "${bindir}/"*; do
+            [ -x "$file" ] && echo "${file##*/}"
+          done
+        done
+      fi
+    done
+  done
+}
+
+make_shims $(list_executable_names | sort -u)
